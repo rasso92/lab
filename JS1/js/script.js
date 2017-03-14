4925f62acd17885f437db1f9ebcaf185cf31a278
System Administrator <root@Alejandro-Sanchez.local>
2017-03-13 20:36:11 -0600

    JS1

---
+++ JS1/js/script.js
@@ -0,0 +1,7 @@
+document.getElementById("discreto").onclick = function() {mensaje()};
+/*function mensaje(){
+  alert("Que tengas una buena semana!");
+}*/
+var mensaje = function() { // funcion anonima
+  alert("Que tengas una buena semana!");
+};
